96e32a599b26a5deb6777d5cbb30e31db27c7a82
Andy Moores <andymoores@tetsuyas-mbp-2.lan>
2024-07-12 12:34:39 -0600

    adding requirments and deploy script

---
+++ deploy.ps1
@@ -0,0 +1,5 @@
+#pip freeze > requirements.txt
+Set-Location C:\Users\admin\Desktop\Development\Python\ZipEstimator
+git pull origin main
+#python -m pip install --upgrade pip
+pip install -r requirements.txt
